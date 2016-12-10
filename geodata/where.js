2e0bffcb3bc4ffb965b6b033a0492cc7d94173e5
xiaoweige <ge.xiaowei@outlook.com>
2016-12-10 22:27:35 +0800

    Visualizing Networks and Interconnections

---
+++ geodata/where.js
@@ -0,0 +1,7 @@
+myData = [
+[45.1864597,9.1555362, 'Università degli Studi di Pavia Biblioteca Boezio, Corso Str. Nuova, 106/c, 27100 Pavia PV, Italy'],
+[47.6498128,-122.3037817, 'University of Washington Station, 3720 Montlake Blvd NE, Seattle, WA 98195, USA'],
+[-7.2149243,-35.9084272, 'Universidade Federal de Campina Grande - R. Aprígio Veloso, 882 - Bodocongó, Campina Grande - PB, 58429-900, Brazil'],
+[19.3188895,-99.1843676, 'University City, Mexico City, CDMX, Mexico'],
+[42.9097484,-85.7630885, 'Grandville, MI, USA']
+];
